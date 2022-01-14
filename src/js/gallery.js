1b7b42cde44e4f2a3ae92572e449377ef54887dd
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-01-14 15:51:17 +0200

    1

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,21 +1,46 @@
-import ref from './Refs';
-import movieCardTpl from '../templates/fetchMovieTemplate.hbs';
-import ApiService from './FetchMovie';
-const API_KEY = new ApiService();
-const BASE_URL = 'https://api.themoviedb.org/3';
+// import ref from './Refs';
+// import movieCardTpl from '../templates/fetchMovieTemplate.hbs';
+// import ApiService from './FetchMovie';
+// const apiService = new ApiService();
+// const BASE_URL = 'https://api.themoviedb.org/3';
 
-fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=b59f17e26fe0bb62f0ad4e21eca227b9')
-  .then(response => {
-    return response.json();
-  })
-  .then(films => {
-    const murkup = movieCardTpl(films.results);
-    ref.galleryRef.innerHTML = murkup;
-  });
-const genre = fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+// fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiService}`)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(films => {
+//     const murkup = movieCardTpl(films.results);
+//     ref.galleryRef.innerHTML = murkup;
+//   });
+// const genre = fetch(`${BASE_URL}/genre/movie/list?api_key=${apiService}&language=en-US`)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   });
+import FetchMovieHeader from './FetchMovie';
+import movieTpl from '../templates/fetchMovieTemplate.hbs';
+const fetchMovieHeader = new FetchMovieHeader();
+import ref from './Refs';
+
+ref.mainFilmSerch.addEventListener('gallery', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  fetchMovieHeader.query = e.currentTarget.elements.query.value.trim();
+  clearFetchResault();
+  if (fetchMovieHeader.query === '') {
+    clearFetchResault();
+  } else {
+    fetchMovieHeader.searchfetchMovieGenres().then(renderMovieList);
+  }
+}
+
+function renderMovieList(data) {
+  ref.galleryRef.insertAdjacentHTML('afterbegin', `<ul>${movieTpl(data)}</ul>`);
+}
+
+function clearFetchResault() {
+  ref.galleryRef.innerHTML = '';
+}

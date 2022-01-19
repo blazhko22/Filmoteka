adb918882a38e013d2e205144e4d37ff777f8212
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-01-19 16:10:43 +0200

    11

--- src/js/paginationSerchFilm.js
+++ src/js/paginationSerchFilm.js
@@ -4,49 +4,21 @@ import DataFetch from './filmServiceApi.js';
 import { renderMovieCardFilms } from './normaliseRenderFilm';
 const dataFetch = new DataFetch();
 
-// function tuiPaginationSerch() {
-//   dataFetch.page = 0;
-//   const pagination2 = new Pagination(refs.paginationRef, {
-//     totalItems: DataFetch.totalPages,
-//     itemsPerPage: 20,
-//     visiblePages: 5,
-//     centerAlign: true,
-//   });
-//   pagination2.on('beforeMove', event => {
-//     dataFetch.page = event.page;
-//     dataFetch.fetchFilms().then(films => {
-//       refs.galleryRef.innerHTML = '';
-//       renderMovieCardFilms(films);
-//     });
-//   });
-//   console.log(pagination2);
-// }
-
-// export { tuiPaginationSerch };
-
 function tuiPaginationSerch() {
-  dataFetch.page = 0;
-
-  const pagination = new Pagination(refs.paginationRef, {
+  dataFetch.page = 1;
+  const pagination2 = new Pagination(refs.paginationRef, {
     totalItems: DataFetch.totalPages,
     itemsPerPage: 20,
     visiblePages: 5,
     centerAlign: true,
   });
-
-  pagination.on('beforeMove', function (eventData) {
-    const params = new URLSearchParams(window.location.search);
-    params.set('page', eventData.page);
-    history.pushState(null, null, '?' + params.toString());
-  });
-
-  pagination.on('afterMove', function (eventData) {
-    if (pagination._options.totalItems <= 20) {
+  pagination2.on('beforeMove', event => {
+    dataFetch.page = event.page;
+    dataFetch.fetchFilms().then(films => {
       refs.galleryRef.innerHTML = '';
-    }
+      renderMovieCardFilms(films);
+    });
   });
-  console.log(pagination);
-  return pagination;
 }
 
 export { tuiPaginationSerch };
--- src/js/serchFilmByValue.js
+++ src/js/serchFilmByValue.js
@@ -1,34 +1,32 @@
-import DataFetch from './filmServiceApi.js';
+import DataFetch from "./filmServiceApi.js";
 import { renderMovieCardFilms } from './normaliseRenderFilm';
 const dataFetch = new DataFetch();
 import ref from './Refs';
-import { showLoader, hideLoader } from './loader';
+import { showLoader, hideLoader } from './loader'
 import renderPopUpContent from './renderPopUpContent';
-import { tuiPaginationSerch } from './paginationSerchFilm';
 
 ref.buttonFilmSerch.disabled = true;
 
 ref.formFilmSerch.addEventListener('submit', onSearch);
 ref.inputFilmSerch.addEventListener('input', onSearchInput);
-function onSearch(e) {
+function onSearch (e)  {
   e.preventDefault();
   dataFetch.query = e.currentTarget.elements.query.value.trim();
-  dataFetch
-    .fetchFilms()
-    .then(renderMovieCardFilms)
-    .then(renderPopUpContent)
-    .catch(error => {
-      console.log(error.message);
-    });
-  tuiPaginationSerch();
-}
+    dataFetch
+      .fetchFilms()
+      .then(renderMovieCardFilms).then(renderPopUpContent)
+      .catch(error => {
+        console.log(error.message);
+      });
+};
 
 function onSearchInput(e) {
   e.preventDefault();
   if (!e.currentTarget.value) {
     ref.buttonFilmSerch.disabled = true;
-    dataFetch.fetchTopFilms().then(renderMovieCardFilms);
-  } else {
+    dataFetch.fetchTopFilms().then(renderMovieCardFilms)
+  }
+  else {
     ref.buttonFilmSerch.disabled = false;
   }
 }
